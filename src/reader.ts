054a012efad8fdd75a24022b637c3b0198761f3e
Stefan <stefan.puia13@gmail.com>
2021-04-10 23:09:55 +0100

    parse time to reader

--- src/reader.ts
+++ src/reader.ts
@@ -1,13 +1,24 @@
 import { Converter } from "./core/converter";
-import { readFile } from "fs";
+import { readFile, writeFileSync } from "fs";
 import { join } from "path";
 import { MethodMode } from "./types";
 
+const [, start] = process.hrtime();
 readFile(join(__dirname, "../read.xml"), "utf-8", (err, data) => {
     if (err) {
         console.error(err);
     } else {
         console.clear();
-        console.log(Converter.convert(data, MethodMode.SERVICE));
+        writeFileSync(
+            join(__dirname, "../out.java"),
+            Converter.convert(data, MethodMode.SERVICE)
+        );
+        const [, end] = process.hrtime();
+        console.log(
+            `Finished parsing ${data.split("\n").length} lines in ${(
+                (end - start) /
+                Math.pow(10, 9)
+            ).toFixed(4)} seconds.`
+        );
     }
 });
