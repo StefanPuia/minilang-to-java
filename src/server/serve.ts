7338ccf61a275d04b6aed9d37c7d892b693d9141
Stefan <stefan.puia13@gmail.com>
2021-04-14 19:27:30 +0100

    added better error handling

--- src/server/serve.ts
+++ src/server/serve.ts
@@ -1,19 +1,27 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import { join } from "path";
 import { Converter } from "../core/converter";
-import { MethodMode } from '../types';
 
 dotenv.config();
 
 const port = process.env.PORT ?? 5055;
 const app = express();
 
-app.use(express.json({
-    limit: "5mb"
-}));
+app.use(
+    express.json({
+        limit: "5mb",
+    })
+);
 
-app.get("/", (req: Request, res: Response) => {
+app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
+    if (err) {
+        res.json({ output: err.toString() });
+    }
+    return next(err);
+});
+
+app.get("/", (_req: Request, res: Response) => {
     res.sendFile(join(__dirname, "../../public/index.html"));
 });
 
@@ -23,7 +31,7 @@ app.post("/convert", async (req, res) => {
             req.body.input,
             req.body.methodMode,
             req.body.packageName,
-            req.body.className,
+            req.body.className
         );
         res.json({
             output: output,
